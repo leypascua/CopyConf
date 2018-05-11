7636026731cacf5df3f3427241e84b242206235b
Harley Pascua <leypascua@yahoo.com>
2018-05-11 11:08:33 +0800

    multi-target netcoreapp2.0 and net40 (#1)
    cleanup files

--- src/CopyConf/AppSettings.cs
+++ src/CopyConf/AppSettings.cs
@@ -1,29 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Configuration;
-using System.Linq;
-using System.Text;
 
 namespace CopyConf
 {
     public class AppSettings
     {
+        const string CopyConfIncludedFilesKey = "CopyConf:Include";
+        const string DefaultSupportedFiles = ".conf;.config;.json;.yaml;.yml;.ini;";
+
         static AppSettings()
         {
-            Current = CreateInstance(ConfigurationManager.AppSettings);
+            string includeVal = (Environment.GetEnvironmentVariable(CopyConfIncludedFilesKey) ?? string.Empty)
+                .Trim();
+
+            Current = new AppSettings
+            {
+                Include = string.IsNullOrEmpty(includeVal) ? DefaultSupportedFiles : includeVal
+            };
         }
 
         public static AppSettings Current { get; private set; }
 
-        public string Include { get; private set; }
-
-        public static AppSettings CreateInstance(NameValueCollection settings)
-        {
-            return new AppSettings
-            {
-                Include = settings["Include"] ?? ".json;.config;.conf;.cfg;.yaml"
-            };
-        }
+        public string Include { get; internal set; }
+        
     }
 }
